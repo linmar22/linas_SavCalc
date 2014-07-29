58833566c9d5f2871854367a1469a4e33bea641b
Linas <Linas@Linas-Dell>
2014-07-29 09:34:17 +0300

    Fixed russian translation semantics error. Changed table cells to un-editable.

--- src/controller/TablePopulator.java
+++ src/controller/TablePopulator.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package controller;
 
 import java.text.ParseException;
@@ -20,37 +19,41 @@ import model.Operation;
  * @author Linas
  */
 public class TablePopulator {
-    
+
     JTable table;
     ArrayList<Operation> opList;
     DataAccess da = DataAccess.getInstance();
     int rows;
     String columns[] = {"Operation ID", "Invoice ID", "Date", "Quantity", "Price", "Stock ID"};
-    
+
     public TablePopulator(JTable table) {
         this.table = table;
     }
-    
-    public TablePopulator(){
-        
+
+    public TablePopulator() {
+
     }
-    
+
     /**
      * Populates a table with Operation objects.
-     * 
+     *
      * @param table the JTable to be populated.
      */
     public void populateTable(JTable table) {
-        
+
         JTable operationTable = table;
         ArrayList<Operation> opL = da.getOperationList();
         rows = da.getOperationList().size();
-        
-        
-        DefaultTableModel dtm = new DefaultTableModel(columns, 0);
 
-        for (Operation o:opL) {
-            
+        DefaultTableModel dtm = new DefaultTableModel(columns, 0) {
+            @Override
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+
+        for (Operation o : opL) {
+
             int operationID = o.getId();
             int invoiceID = o.getInvoice_id();
             Date unformattedDate = da.toDateObject(o.getDate());
@@ -64,22 +67,22 @@ public class TablePopulator {
         }
         operationTable.setModel(dtm);
     }
-    
+
     /**
-     * 
+     *
      * @return a String[] representation of the table columns.
      */
     public String[] getColumns() {
         return columns;
     }
-    
+
     /**
      * Sets a list of columns to be shown to the user.
-     * @param columns 
+     *
+     * @param columns
      */
     public void setColumns(String[] columns) {
         this.columns = columns;
     }
-    
-    
+
 }
